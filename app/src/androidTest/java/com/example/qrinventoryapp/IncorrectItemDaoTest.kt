230eb5cfa73c83df9c1691245661d9eb247a2232
Michal Dvorsky <michal.dvorsky80@gmail.com>
2025-08-04 12:53:14 +0200

    BaseDaoTest added and Dao tests rewritten

--- app/src/androidTest/java/com/example/qrinventoryapp/IncorrectItemDaoTest.kt
+++ app/src/androidTest/java/com/example/qrinventoryapp/IncorrectItemDaoTest.kt
@@ -1,59 +1,19 @@
 package com.example.qrinventoryapp
 
-import android.content.Context
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.qrinventoryapp.data.IncorrectItem
 import com.example.qrinventoryapp.data.IncorrectItemDao
-import com.example.qrinventoryapp.data.InventoryDatabase
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.After
 import org.junit.Assert.assertTrue
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import java.io.IOException
 
-@RunWith(AndroidJUnit4::class)
-class IncorrectItemDaoTest {
+class IncorrectItemDaoTest : BaseDaoTest( ){
 
     private lateinit var incorrectItemDao: IncorrectItemDao
-    private lateinit var inventoryDatabase: InventoryDatabase
 
-    @Before
-    fun createDb() {
-        val context: Context = ApplicationProvider.getApplicationContext()
-
-        val testDbFile = context.getDatabasePath("test_inventory.db")
-        if (testDbFile.exists()) testDbFile.delete()
-
-        context.assets.open("database/inventory.db").use { inputStream ->
-            testDbFile.outputStream().use { outputStream ->
-                inputStream.copyTo(outputStream)
-            }
-        }
-
-        inventoryDatabase = Room.databaseBuilder(
-            context,
-            InventoryDatabase::class.java,
-            "test_inventory.db"
-        )
-            .allowMainThreadQueries()
-            .build()
-
-        //inventoryDatabase = Room.inMemoryDatabaseBuilder(context, InventoryDatabase::class.java)
-        //    .allowMainThreadQueries()
-        //    .build()
+    override fun afterCreatingDatabaseFun() {
         incorrectItemDao = inventoryDatabase.incorrectItemDao()
-    }
-
-    @After
-    @Throws(IOException::class)
-    fun closeDb() {
-        inventoryDatabase.close()
     }
 
     private var incorrectItem1 = IncorrectItem(1, "ABC123", "11.5.1980", 1, 2, 1, 1)
